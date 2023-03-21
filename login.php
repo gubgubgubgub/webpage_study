07dbe248446a8632a5a37b9e50e5d925015a8617
Liam <gubs5398@gmail.com>
2023-03-21 12:21:20 +0900

    to fix login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ $username = "Liam";
 $password = "rudwns5398";
 $dbname = "hi";
 
-$conn = new mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // 오류 확인
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
@@ -14,9 +14,13 @@ if ($conn->connect_error) {
 }
 
 $ID = $_POST['ID'];
-$password = $_POST['password'];
+$password = $_POST[ 'password'];
 
-$sql = "SELECT * FROM exam WHERE identification = ?"; // "exam" 테이블을 사용하고 "id" 컬럼을 사용한다고 가정
+// 확인을위한 코드
+echo "Client ID: " . $ID . "\n";
+echo "Client Password: " . $password . "\n";
+
+$sql = "SELECT * FROM exam WHERE identification = ?"; 
 $stmt = $conn->prepare($sql);
 $stmt->bind_param('s', $ID);
 $stmt->execute();
@@ -24,7 +28,11 @@ $result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
     $row = $result->fetch_assoc();
-    if (password_verify($password, $row['password'])) { // "password" 컬럼을 사용한다고 가정
+    // 확인을위한 코드
+    echo "Database ID: " . $row['identification'] . "\n";
+    echo "Database Password: " . $row['password'] . "\n";
+
+    if (password_verify($password, $row['password'])) { 
         echo 'success';
     } else {
         echo 'fail';
