c0e4359d1c60a087f2df414d143cf4bed562a6c8
Liam <gubs5398@gmail.com>
2023-03-28 13:26:05 +0900

    add post .php file

---
+++ get_posts.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+$conn = mysqli_connect("localhost", "Liam", "rudwns5398", "notic_table");
+if (mysqli_connect_errno($conn)) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM notice_table1 ORDER BY createdAt DESC";
+$result = mysqli_query($conn, $sql);
+
+$posts = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $posts[] = $row;
+}
+
+echo json_encode($posts);
+mysqli_close($conn);
+?>
