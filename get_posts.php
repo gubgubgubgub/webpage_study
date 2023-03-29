d4cd77e750632a755aef04583777603f28ebc394
Liam <gubs5398@gmail.com>
2023-03-30 05:00:11 +0900

    log

--- get_posts.php
+++ get_posts.php
@@ -14,14 +14,6 @@ while ($row = mysqli_fetch_assoc($result)) {
     $posts[] = $row;
 }
 
-foreach ($posts as $post) { // foreach는 배열의 요소를 하나씩 순회하며 값을 가져오는 반복문 as 키워드를 사용하여 배열의 각 요소를 $post라는 변수에 할당
-    echo "id: " . $post['id'] . "<br>";
-    echo "title: " . $post['title'] . "<br>";
-    echo "content: " . $post['content'] . "<br>";
-    echo "writer: " . $post['writer'] . "<br>";
-    echo "createdAt: " . $post['createdAt'] . "<br>";
-}
-
-//echo json_encode($posts);
+echo json_encode($posts);
 mysqli_close($conn);
 ?>
